6b11e0bc15ff212185e569a6714900f742b85ed9
nrm <sanew102@gmail.com>
2022-04-30 18:10:35 +0600

    add TabBar with 4 views

---
+++ Unsplash/PresentationLayer/SearchView.swift
@@ -0,0 +1,29 @@
+//
+//  SearchView.swift
+//  Unsplash
+//
+//  Created by Нурым Нагиметов on 30.04.2022.
+//
+
+import UIKit
+
+class SearchView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
