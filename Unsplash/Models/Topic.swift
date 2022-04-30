868d571882dab3470f5152e1cc2dcda2472c682a
Kassymzhan <g.kasymzhan@mail.ru>
2022-05-01 02:07:32 +0600

    topic service added

---
+++ Unsplash/Models/Topic.swift
@@ -0,0 +1,15 @@
+//
+//  Topic.swift
+//  Unsplash
+//
+//  Created by Касымжан Гиждуан on 01.05.2022.
+//
+
+import Foundation
+
+struct Topic: Decodable {
+    let id: String
+    let slug: String
+    let title: String
+    let description: String
+}
